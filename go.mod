85230154ac83dd65070fbb9aacb1142012c783aa
tangchenhao.trainee <tangchenhao.trainee@bytedance.com>
2022-03-28 16:37:44 +0800

    zk claim

--- go.mod
+++ go.mod
@@ -5,5 +5,6 @@ go 1.16
 require (
 	github.com/consensys/gnark v0.5.2
 	github.com/consensys/gnark-crypto v0.5.3
+	github.com/gin-gonic/gin v1.7.7
 	github.com/go-sql-driver/mysql v1.6.0
 )
--- server.go
+++ server.go
@@ -1,98 +1,202 @@
 package main
 
 import (
-	"crypto/sha256"
+	"bytes"
 	"database/sql"
 	"encoding/hex"
 	"encoding/json"
+	"fmt"
+	"github.com/Stype0912/DCanDID/zk"
+	"github.com/consensys/gnark-crypto/ecc"
+	"github.com/consensys/gnark/backend/groth16"
 	_ "github.com/go-sql-driver/mysql"
 	"net/http"
 	"time"
 )
 
-type UserInfo struct {
-	UserName string `json:"user_name"`
-	PassWord string `json:"pass_word"`
+type UserOriginInfo struct {
+	Id string `json:"id"`
 }
 
-type ResponseInfo struct {
-	IsPass string `json:"is_pass"`
+type Claim []struct {
+	Attr       string `json:"attr"`
+	Commitment string `json:"commitment"`
+	Provider   string `json:"provider"`
 }
 
-func signIn(w http.ResponseWriter, request *http.Request) {
+type ClaimResp struct {
+	StatusCode int64  `json:"status_code"`
+	Message    string `json:"message"`
+	Claim      Claim  `json:"claim"`
+	Id         string `json:"id"`
+	Witness    string `json:"witness"`
+	IsNew      bool   `json:"is_new"`
+}
+
+type Witness struct {
+	VerifyingKey  string `json:"verifying_key"`
+	PublicWitness string `json:"public_witness"`
+}
+
+func OracleGetCommit(w http.ResponseWriter, request *http.Request) {
 	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.WriteHeader(http.StatusOK)
 	decoder := json.NewDecoder(request.Body)
 
-	var userInfo UserInfo
-	var responseInfo ResponseInfo
+	var userInfo UserOriginInfo
+	var responseInfo ClaimResp
 
 	_ = decoder.Decode(&userInfo)
 
-	db, err := sql.Open("mysql", "root:tang2000912@tcp(127.0.0.1:3306)/user_info?charset=utf8")
+	db, err := sql.Open("mysql", "root:tang2000912@tcp(127.0.0.1:3306)/user_information?charset=utf8")
 	if err != nil {
-		responseInfo.IsPass = "数据库链接失败，请重试"
+		responseInfo.StatusCode = 1
+		responseInfo.Message = "数据库连接失败，请重试"
 	}
 
-	var user_name, password string
-	_ = db.QueryRow("SELECT user_name, password FROM user_information WHERE user_name = ?", userInfo.UserName).Scan(&user_name, &password)
+	var witness string
+	var claim string
+	var isClaimed bool
+	_ = db.QueryRow("SELECT claim, is_claimed, witness FROM user_information WHERE user_id = ?", userInfo.Id).Scan(&claim, &isClaimed, &witness)
 
-	if user_name == "" {
-		responseInfo.IsPass = "尚未注册，请先注册"
+	var claimStruct Claim
+	var witnessStruct Witness
+	if isClaimed {
+		err = json.Unmarshal([]byte(claim), &claimStruct)
+		responseInfo.Claim = claimStruct
+		responseInfo.Id = userInfo.Id
+		responseInfo.Witness = witness
+		responseInfo.IsNew = false
 	} else {
-		if password == Encode(userInfo.PassWord) {
-			responseInfo.IsPass = "校验通过"
-		} else {
-			responseInfo.IsPass = "密码错误"
-		}
-	}
-	if err := json.NewEncoder(w).Encode(responseInfo); err != nil {
-		panic(err)
-	}
-}
+		com := zk.Commitment(userInfo.Id)
 
-func signUp(w http.ResponseWriter, request *http.Request) {
-	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-	w.WriteHeader(http.StatusOK)
-	decoder := json.NewDecoder(request.Body)
-
-	var userInfo UserInfo
-	var responseInfo ResponseInfo
-
-	_ = decoder.Decode(&userInfo)
-
-	db, err := sql.Open("mysql", "root:tang2000912@tcp(127.0.0.1:3306)/user_info?charset=utf8")
-	if err != nil {
-		responseInfo.IsPass = "数据库链接失败，请重试"
-	}
-
-	var user_name, password string
-	_ = db.QueryRow("SELECT user_name, password FROM user_information WHERE user_name = ?", userInfo.UserName).Scan(&user_name, &password)
-	if user_name != "" {
-		responseInfo.IsPass = "已注册，请登陆"
-	} else {
-		_, err := db.Exec("INSERT INTO user_information (user_name, password, submission_date) VALUES (?, ?, ?)", userInfo.UserName, Encode(userInfo.PassWord), time.Now().Format("2006-01-02 15:04:05"))
+		a := bytes.NewBuffer([]byte(""))
+		_, err = com.Proof.WriteTo(a)
 		if err != nil {
-			responseInfo.IsPass = "注册失败，请重试"
-		} else {
-			responseInfo.IsPass = "注册成功，请登陆"
+			return
 		}
+		proof := hex.EncodeToString(a.Bytes())
+
+		b := bytes.NewBuffer([]byte(""))
+		_, err = com.VerifyingKey.WriteTo(b)
+		if err != nil {
+			return
+		}
+		verifyingKey := hex.EncodeToString(b.Bytes())
+
+		c, err := json.Marshal(com.PublicWitness)
+		publicWitness := string(c)
+
+		if err != nil {
+			responseInfo.StatusCode = 1
+			responseInfo.Message = "marshal错误，请重试"
+			return
+		}
+		witnessStruct.VerifyingKey = verifyingKey
+		witnessStruct.PublicWitness = publicWitness
+		witnessByte, err := json.Marshal(witnessStruct)
+		witnessStr := hex.EncodeToString(witnessByte)
+		if err != nil {
+			responseInfo.StatusCode = 1
+			responseInfo.Message = "marshal错误，请重试"
+			return
+		}
+
+		responseInfo.Id = userInfo.Id
+		responseInfo.Claim = Claim{
+			{
+				Attr:       "id",
+				Commitment: proof,
+				Provider:   "gov.cn",
+			},
+		}
+		responseInfo.IsNew = true
+		responseInfo.Witness = witnessStr
+		claimStr, _ := json.Marshal(responseInfo.Claim)
+		_, err = db.Exec("INSERT INTO user_information (user_id, claim, is_claimed, witness, submission_date) VALUES (?, ?, ?, ?, ?)",
+			userInfo.Id, claimStr, true, witnessStr, time.Now().Format("2006-01-02 15:04:05"))
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
 	}
-	if err := json.NewEncoder(w).Encode(responseInfo); err != nil {
-		panic(err)
-	}
+	defer func() {
+		json.NewEncoder(w).Encode(responseInfo)
+	}()
 }
 
-func Encode(password string) (encodedPassword string) {
-	h := sha256.New()
-	h.Write([]byte(password))
-	sum := h.Sum(nil)
-	return hex.EncodeToString(sum)
+type VerifyResp struct {
+	IsValid bool `json:"is_valid"`
 }
+
+func CommitmentVerify(w http.ResponseWriter, request *http.Request) {
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.WriteHeader(http.StatusOK)
+	decoder := json.NewDecoder(request.Body)
+
+	var userInfo UserOriginInfo
+	var responseInfo VerifyResp
+
+	_ = decoder.Decode(&userInfo)
+
+	db, _ := sql.Open("mysql", "root:tang2000912@tcp(127.0.0.1:3306)/user_information?charset=utf8")
+
+	var err error
+	var witness string
+	var claim string
+	var isClaimed bool
+	_ = db.QueryRow("SELECT claim, is_claimed, witness FROM user_information WHERE user_id = ?", userInfo.Id).Scan(&claim, &isClaimed, &witness)
+
+	var claimStruct Claim
+	var witnessStruct Witness
+	if isClaimed {
+		// claim以序列化的结构体存储，直接unmarshal即可
+		err = json.Unmarshal([]byte(claim), &claimStruct)
+		e, _ := hex.DecodeString(claimStruct[0].Commitment)
+		proof := groth16.NewProof(ecc.BN254)
+		_, err = proof.ReadFrom(bytes.NewReader(bytes.NewBuffer(e).Bytes()))
+		if err != nil {
+			return
+		}
+
+		// witness以hex存储，先解密，再反序列化
+		witnessByte, _ := hex.DecodeString(witness)
+		err = json.Unmarshal(witnessByte, &witnessStruct)
+		if err != nil {
+			return
+		}
+
+		// vk以hex格式存储，先解密，再读
+		vk := groth16.NewVerifyingKey(ecc.BN254)
+		f, _ := hex.DecodeString(witnessStruct.VerifyingKey)
+		_, err = vk.ReadFrom(bytes.NewReader(bytes.NewBuffer(f).Bytes()))
+		if err != nil {
+			return
+		}
+
+		// pk以序列化结构体存储，直接反序列化
+		var pk *zk.Circuit
+		err = json.Unmarshal([]byte(witnessStruct.PublicWitness), &pk)
+		if err != nil {
+			return
+		}
+		commitment := zk.Com{
+			Proof:         proof,
+			PublicWitness: pk,
+			VerifyingKey:  vk,
+		}
+		responseInfo.IsValid = zk.CommitmentVerify(commitment)
+	}
+	defer func() {
+		json.NewEncoder(w).Encode(responseInfo)
+	}()
+}
+
 func main() {
-	http.HandleFunc("/signin", signIn)
-	http.HandleFunc("/signup", signUp)
-	_ = http.ListenAndServe(":8090", nil)
+	http.HandleFunc("/signin", OracleGetCommit)
+	http.HandleFunc("/check", CommitmentVerify)
+	_ = http.ListenAndServe(":7890", nil)
 }
